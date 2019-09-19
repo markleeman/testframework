71839be5f75d05e52618bc5aa0a0a48f2b6c19af
Mark Leeman <mark.leeman@arm.com>
2019-09-19 16:20:57 +0100

    Added example maven profile to specify system properties, and a test to create the driver from them

--- src/main/java/tests/ExampleTests.java
+++ src/main/java/tests/ExampleTests.java
@@ -14,13 +14,29 @@ public class ExampleTests {
 
     private DriverWrapper driver;
 
-    @BeforeClass
-    public void setup() {
-        driver = new DriverWrapper(DriverWrapper.browsers.CHROME_HEADLESS, DriverWrapper.operatingSystems.LOCAL);
-    }
-
+    // Example test using an explicitly defined browser and os
     @Test
     public void passwordReset(){
+
+        driver = new DriverWrapper(DriverWrapper.browsers.CHROME_HEADLESS, DriverWrapper.operatingSystems.LOCAL);
+
+        String confirmMessage = "Your e-mail's been sent!";
+
+        User testUser = User.createNewRandomUser();
+
+        EmailSent reset = new ForgotPassword(driver)
+                .enterEmail(testUser.getEmailAddress())
+                .submitFormWithButton();
+
+        assertTrue(reset.getMessageText().contains(confirmMessage));
+    }
+
+    // Example test using system properties to specify the browser and os
+    @Test
+    public void test2(){
+
+        // TODO Test something else...
+        driver = DriverWrapper.createFromSystemProperties();
 
         String confirmMessage = "Your e-mail's been sent!";
 
