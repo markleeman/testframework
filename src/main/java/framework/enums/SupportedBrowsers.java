adef91e826385e3596350557c2393150c0d5e6eb
Mark Leeman <mark.leeman@arm.com>
2022-04-20 15:45:50 +0100

    Replaced deprecated BrowserType class

--- src/main/java/framework/enums/SupportedBrowsers.java
+++ src/main/java/framework/enums/SupportedBrowsers.java
@@ -1,16 +1,20 @@
 package framework.enums;
 
+import org.openqa.selenium.remote.Browser;
 import org.openqa.selenium.remote.BrowserType;
 
 /**
  * Web browsers supported by the framework
  */
+
+// TODO: Most/all browsers should now support headless mode, so we should make this an option rather than specified browser type
+
 public enum SupportedBrowsers {
-    FIREFOX (BrowserType.FIREFOX),
-    CHROME (BrowserType.CHROME),
+    FIREFOX (Browser.FIREFOX.browserName()),
+    CHROME (Browser.CHROME.browserName()),
     CHROME_HEADLESS ("chrome_headless"),
-    SAFARI (BrowserType.SAFARI),
-    EDGE (BrowserType.EDGE);
+    SAFARI (Browser.SAFARI.browserName()),
+    EDGE (Browser.EDGE.browserName());
 
     public final String browserName;
 
